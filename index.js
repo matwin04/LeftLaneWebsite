a9e596f16d9ea61eb9511ab952b3e39ec8e94f4f
frostie <mattwiner04@gmail.com>
2025-03-05 10:52:14 -0800

    Updated CSS

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const path = require('path');
-const { engine } = require('express-handlebars');
-const axios = require('axios');
+const express = require("express");
+const path = require("path");
+const { engine } = require("express-handlebars");
+const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 8088;
 
 // Configure Handlebars to use `.html` instead of `.hbs`
-app.engine('html', engine({ extname: '.html', defaultLayout: false }));
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("html", engine({ extname: ".html", defaultLayout: false }));
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "views"));
 
 // Serve static files (CSS, JS, JSON)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Home Route
-app.get('/', (req, res) => res.render('index', { title: 'LeftLane Community' }));
-app.get('/events', (req, res) => res.render('events', { title: 'Events' }));
-app.get('/contact', (req, res) => res.render('contact', { title: 'Contact Us' }));
+app.get("/", (req, res) => res.render("index", { title: "LeftLane Community" }));
+app.get("/events", (req, res) => res.render("events", { title: "Events" }));
+app.get("/contact", (req, res) => res.render("contact", { title: "Contact Us" }));
 
 // Vercel support: Export Express app
 module.exports = app;
